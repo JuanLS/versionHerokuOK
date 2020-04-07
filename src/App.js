a9726c598f19bd6d6eaa42cd9f5a5c9dc02e7577
Juan Gonzalez <juaneusebioge@gmail.com>
2020-04-07 14:25:22 +0100

    Personalizo minimamente modificando index.html y App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenid@ a LatinSalsa
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.latinsalsa.es"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visitanos
         </a>
       </header>
     </div>
